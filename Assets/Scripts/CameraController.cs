6e79b020d6701cac5edbf674450bd20285de7872
jordan-grey <j2reedy17@gmail.com>
2024-05-31 11:54:21 -0700

    clamped camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public float rotationSpeed = 500.0f;
+    public float camMin = -45.0f;
+    public float camMax = 50.0f;
+    private float pitch = 0.0f;
+    private float yaw = 0.0f;
 
     // Update is called once per frame
     void Update()
@@ -17,12 +21,17 @@ public class NewBehaviourScript : MonoBehaviour
 
     private void CamOrbit()
     {
-        if(Input.GetAxis("Mouse Y") != 0 || Input.GetAxis("Mouse X") != 0)
-        {
-            float verticalInput = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
-            float horizontalInput = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
-            transform.Rotate(Vector3.right, -verticalInput);
-            transform.Rotate(Vector3.up, horizontalInput, Space.World);
-        }
+        // if(Input.GetAxis("Mouse Y") != 0 || Input.GetAxis("Mouse X") != 0)
+        // {
+        //     float verticalInput = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+        //     float horizontalInput = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        //     transform.Rotate(Vector3.right, -verticalInput);
+        //     transform.Rotate(Vector3.up, horizontalInput, Space.World);
+        // }
+
+        yaw += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+        pitch += Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+        pitch = Mathf.Clamp(pitch, camMin, camMax);
+        transform.eulerAngles = new Vector3(-pitch, yaw, 0.0f);
     }
 }
